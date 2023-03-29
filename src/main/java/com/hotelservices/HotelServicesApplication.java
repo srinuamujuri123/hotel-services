fa22c42efd19ab3b4a749bd5ba4558e3d5024e31
S.Sajid <shaiksajid492@gmail.com>
2023-03-29 23:34:09 +0530

    a

--- src/main/java/com/hotelservices/HotelServicesApplication.java
+++ src/main/java/com/hotelservices/HotelServicesApplication.java
@@ -1,10 +1,10 @@
 package com.hotelservices;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HotelServicesApplication {
+import com.tms.application.TMSApplicationStarup;
+
+public class HotelServicesApplication extends TMSApplicationStarup {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelServicesApplication.class, args);
